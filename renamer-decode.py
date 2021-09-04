c31d62cba5a9a4e522ae3ebbcf623a2b256fa6b5
suryandana <55724148+suryandana@users.noreply.github.com>
2021-09-04 09:27:53 +0800

    Add files via upload

---
+++ renamer-decode.py
@@ -0,0 +1,7 @@
+# from array import array
+from pathlib import Path
+
+p = sorted(Path('folder').glob('**/*'))
+for x in p:
+  if x.is_file():
+	x.rename(str(x.parents[0]) + '\\' + bytearray.fromhex(x.name).decode())
